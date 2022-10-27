1f5e87f023b1a7961a6abd31157ef385b87c460a
udaykirankolla <uday7702192162@gmail.com>
2022-10-26 20:16:40 -0500

    new file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
-var computationRouter = require('./routes/computation');
+var computationsRouter = require('./routes/computations');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
-app.use('/computation', computationRouter);
+app.use('/computations', computationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
